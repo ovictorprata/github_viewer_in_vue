3d1a682aec0f067b882beb0ef61f829f8b15e445
Tony Lampada <tonylampada@gmail.com>
2023-01-18 17:34:20 -0300

    busca todos os repos do usuario. atropela a paginacao do github

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,29 @@
+async function fetch_all_pages(url){
+    let vaiindo = true
+    let result = []
+    let page = 1
+    while(vaiindo){
+        const response = await fetch(`${url}?page=${page}`)
+        const tmpitems = await response.json()
+        if(tmpitems.length > 0){
+            result = result.concat(tmpitems)
+            page++
+        } else {
+            vaiindo = false
+        }
+    }
+    return result
+}
+
 export const api = {
     async search_users(searchstring){
         const url = `https://api.github.com/search/users?q=${searchstring}`
-        const result = await fetch(url)
-        return await result.json()
+        const response = await fetch(url)
+        return await response.json()
     },
-    async lista_repos(username, page){
-        if (!page){
-            page = 1
-        }
-        const url = `https://api.github.com/users/${username}/repos?page=${page}`
-        const result = await fetch(url)
-        return await result.json()
+    async lista_repos(username){
+        const url = `https://api.github.com/users/${username}/repos`
+        const data = await fetch_all_pages(url)
+        return data
     }
 }
